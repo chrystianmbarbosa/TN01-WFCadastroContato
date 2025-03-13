c6ae00ce659c3d2c791af81b0a4e1fe4a5b3d43d
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2025-03-13 20:09:29 -0300

    Adicionar arquivos de projeto.

---
+++ TN01-WFCadastroContato/FormContato.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TN01_WFCadastroContato
+{
+    public partial class FormContato : Form
+    {
+        public FormContato()
+        {
+            InitializeComponent();
+        }
+
+        public void Erro(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public void Sucesso(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //Jeito 2 (Mantendo as máscaras do maskedTextBox)
+            string semMaskTelefone = mtbDddTelefone.Text
+                .Replace("(", "")
+                .Replace(")", "")
+                .Replace(" ", "")
+                .Replace("-", "");
+
+            //Verifica Nome Vazio
+            if (string.IsNullOrEmpty(txtNome.Text))
+            {
+                Erro("Campo Nome não pode estar Vazio!");
+                return;
+            }
+            //Verifica SobreNome Vazio
+            else if (string.IsNullOrEmpty(txtSobrenome.Text))
+            {
+                Erro("Campo Sobrenome não pode estar Vazio!");
+                return;
+            }
+            //Verifica DDD e o Telefone Vazios
+            else if (string.IsNullOrEmpty(semMaskTelefone))
+            {
+                Erro("Campo Telefone não pode estar Vazio!");
+                return;
+            }
+            //Verifica Email Vazios
+            else if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                Erro("Campo Email não pode estar Vazio!");
+                return;
+            }
+
+            string tipoTelefone = "";
+            //Se todos os radios estão desmarcados
+            if (!rdbComercial.Checked && !rdbPessoal.Checked && !rdbRecado.Checked)
+            {
+                Erro("Deve-se marcar uma opção de Tipo de Telefone!");
+                return;
+            }
+            else
+            {
+                if (rdbComercial.Checked)
+                    tipoTelefone = "Comercial";
+                else if (rdbPessoal.Checked)
+                    tipoTelefone = "Pessoal";
+                else
+                    tipoTelefone = "Recado";
+            }
+            //Jeito 1 (Retirando as máscaras do maskedTextBox)
+            //string dddTelefone =
+            //    "(" + mtbDddTelefone.Text.Substring(0, 2) + ") "
+            //    + mtbDddTelefone.Text.Substring(2, 5) + "-"
+            //    + mtbDddTelefone.Text.Substring(7);
+
+            
+            string mensagem = @$"
+                    Nome Completo: {txtNome.Text} {txtSobrenome.Text}
+                    Tipo Telefone: {tipoTelefone}
+                    DDD/Telefone: {mtbDddTelefone.Text}
+                    Email: {txtEmail.Text}
+                    ";
+
+            Sucesso(mensagem);
+
+        }
+    }
+}
---
+++ TN01-WFCadastroContato/FormPrincipal.cs
@@ -0,0 +1,26 @@
+namespace TN01_WFCadastroContato
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void mnsCadastrar_Click(object sender, EventArgs e)
+        {
+            FormContato form = new FormContato();
+            form.ShowDialog();
+        }
+
+        private void mnsSobre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mnsSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
